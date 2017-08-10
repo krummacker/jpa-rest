b29bc25dcbad7300de6f5d13df3fb84d270ebd06
Dirk Krummacker <rasen_02cider@icloud.com>
2017-08-10 08:15:18 +0200

    used and tried out the Optional class

---
+++ src/main/java/de/krummacker/optional/EntityRetriever.java
@@ -0,0 +1,19 @@
+package de.krummacker.optional;
+
+import java.util.Optional;
+
+public class EntityRetriever {
+
+    public EntityRetriever() {
+    }
+
+    public Optional<String> createRandomString(int length) {
+        if (length < 0 || length > 10) {
+            return Optional.empty();
+        } else {
+            RandomString rs = new RandomString(length);
+            String s = rs.nextString();
+            return Optional.of(s);
+        }
+    }
+}
