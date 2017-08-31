d605a96fd1ffc3dade069b3ef36ff39a6e5a28ab
Dirk Krummacker <rasen_02cider@icloud.com>
2017-08-31 07:43:37 +0000

    add "count" parameter to "setup" endpoint

--- src/main/java/de/krummacker/jpa/AdminRestController.java
+++ src/main/java/de/krummacker/jpa/AdminRestController.java
@@ -8,7 +8,7 @@ import de.krummacker.jpa.repositories.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,69 +33,63 @@ public class AdminRestController {
     @Autowired
     private DepartmentRepository departmentRepository;
 
-    @Value("${setup.users}")
-    private int setupUsers;
-
     @RequestMapping(value = "/admin/shutdown", method = RequestMethod.PUT)
     public void shutdown() {
         log.info("Received shutdown request. Exiting.");
         System.exit(0);
     }
 
-    @RequestMapping(value = "/admin/setup", method = RequestMethod.PUT)
-    public void setup() {
+    @RequestMapping(value = "/admin/setup/{count}", method = RequestMethod.PUT)
+    public void setup(@PathVariable Integer count) {
 
-        customerRepository.save(new Customer("Jack", "Bauer", Customer.Rating.AVOID));
-        customerRepository.save(new Customer("Chloe", "O'Brian", Customer.Rating.NEW));
-        customerRepository.save(new Customer("Kim", "Bauer", Customer.Rating.TRUSTED));
-        customerRepository.save(new Customer("David", "Palmer", Customer.Rating.AVOID));
-        customerRepository.save(new Customer("Michelle", "Dessler", Customer.Rating.NEW));
-        customerRepository.save(new Customer("Dirk", "Krummacker", Customer.Rating.TRUSTED));
+        for (int i = 0; i < count; ++i) {
 
-        for (int i = 0; i < setupUsers; ++i) {
-            String firstName = UUID.randomUUID().toString();
-            String lastName = UUID.randomUUID().toString();
-            customerRepository.save(new Customer(firstName, lastName, Customer.Rating.NEW));
+            customerRepository.save(new Customer("Jack", UUID.randomUUID().toString(), Customer.Rating.AVOID));
+            customerRepository.save(new Customer("Chloe", UUID.randomUUID().toString(), Customer.Rating.NEW));
+            customerRepository.save(new Customer("Kim", UUID.randomUUID().toString(), Customer.Rating.TRUSTED));
+            customerRepository.save(new Customer("David", UUID.randomUUID().toString(), Customer.Rating.AVOID));
+            customerRepository.save(new Customer("Michelle", UUID.randomUUID().toString(), Customer.Rating.NEW));
+            customerRepository.save(new Customer("Dirk", UUID.randomUUID().toString(), Customer.Rating.TRUSTED));
+
+            DeliveryNote deliveryNote = new DeliveryNote(123456, 122, Calendar.getInstance().getTime());
+            deliveryNote.addItem(new DeliveryNoteItem(deliveryNote, 2, 123456, "Bracelet"));
+            deliveryNote.addItem(new DeliveryNoteItem(deliveryNote, 3, 532734, "Earring"));
+            deliveryNote.addItem(new DeliveryNoteItem(deliveryNote, 2, 343423, "Ring"));
+            deliveryNoteRepository.save(deliveryNote);
+
+            Employee rafferty = new Employee("Jacob", UUID.randomUUID().toString());
+            rafferty.addPhone(new Phone(rafferty, "123456"));
+            rafferty.addPhone(new Phone(rafferty, "647632"));
+            rafferty.addPhone(new Phone(rafferty, "987309"));
+
+            Employee jones = new Employee("Bill", "Jones");
+            Employee heisenberg = new Employee("Werner", "Heisenberg");
+            Employee robinson = new Employee("Herbert", "Robinson");
+            Employee smith = new Employee("Jane", "Smith");
+            Employee williams = new Employee("Linda", "Williams");
+
+            Department sales = new Department("Sales");
+            Department engineering = new Department("Engineering");
+            Department clerical = new Department("Clerical");
+            Department marketing = new Department("Marketing");
+
+            sales.addMember(rafferty);
+            engineering.addMember(jones);
+            engineering.addMember(heisenberg);
+            clerical.addMember(robinson);
+            clerical.addMember(smith);
+
+            departmentRepository.save(sales);
+            departmentRepository.save(engineering);
+            departmentRepository.save(clerical);
+            departmentRepository.save(marketing);
+
+            employeeRepository.save(rafferty);
+            employeeRepository.save(jones);
+            employeeRepository.save(heisenberg);
+            employeeRepository.save(robinson);
+            employeeRepository.save(smith);
+            employeeRepository.save(williams);
         }
-
-        DeliveryNote deliveryNote = new DeliveryNote(123456, 122, Calendar.getInstance().getTime());
-        deliveryNote.addItem(new DeliveryNoteItem(deliveryNote, 2, 123456, "Bracelet"));
-        deliveryNote.addItem(new DeliveryNoteItem(deliveryNote, 3, 532734, "Earring"));
-        deliveryNote.addItem(new DeliveryNoteItem(deliveryNote, 2, 343423, "Ring"));
-        deliveryNoteRepository.save(deliveryNote);
-
-        Employee rafferty = new Employee("Jacob", "Rafferty");
-        rafferty.addPhone(new Phone(rafferty, "123456"));
-        rafferty.addPhone(new Phone(rafferty, "647632"));
-        rafferty.addPhone(new Phone(rafferty, "987309"));
-
-        Employee jones = new Employee("Bill", "Jones");
-        Employee heisenberg = new Employee("Werner", "Heisenberg");
-        Employee robinson = new Employee("Herbert", "Robinson");
-        Employee smith = new Employee("Jane", "Smith");
-        Employee williams = new Employee("Linda", "Williams");
-
-        Department sales = new Department("Sales");
-        Department engineering = new Department("Engineering");
-        Department clerical = new Department("Clerical");
-        Department marketing = new Department("Marketing");
-
-        sales.addMember(rafferty);
-        engineering.addMember(jones);
-        engineering.addMember(heisenberg);
-        clerical.addMember(robinson);
-        clerical.addMember(smith);
-
-        departmentRepository.save(sales);
-        departmentRepository.save(engineering);
-        departmentRepository.save(clerical);
-        departmentRepository.save(marketing);
-
-        employeeRepository.save(rafferty);
-        employeeRepository.save(jones);
-        employeeRepository.save(heisenberg);
-        employeeRepository.save(robinson);
-        employeeRepository.save(smith);
-        employeeRepository.save(williams);
     }
 }
